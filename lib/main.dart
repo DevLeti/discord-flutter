af1ca5263ea1e6b87baee100afcb2a51738087e8
DevLeti <devleti0106@gmail.com>
2023-09-25 10:10:00 +0900

    ADD: assets/prototype - images

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PYD - Promote Your Discord',
+      title: 'FYD - Fit Your Discord',
       theme: ThemeData(
         // This is the theme of your application.
         //
